17f9c8dc205c7d38ee35e6b950b92aa41533da93
Bo4anckii <bobaanton@gmail.com>
2020-11-05 22:34:17 +0300

    Изменил view

---
+++ src/sample/controllers/Controller.java
@@ -0,0 +1,95 @@
+package sample.controllers;
+
+import javafx.application.Platform;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import sample.models.Model;
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Controller implements Initializable {
+
+    public Label text1;
+    public Label text2;
+    public Label text3;
+    public Button stopBtn;
+    private final Model model = new Model();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        newGame();
+    }
+
+    private void newGame(){
+        stopBtn.setText("Остановить первую цифру");
+        stopBtn.setOnMouseClicked(e1 -> {
+            model.getTimer1().cancel();
+            stopBtn.setText("Остановить вторую цифру");
+            stopBtn.setOnMouseClicked(e2 -> {
+                stopBtn.setText("Остановить третью цифру");
+                model.getTimer2().cancel();
+                stopBtn.setOnMouseClicked(e3 -> {
+                    model.getTimer3().cancel();
+                    if (text1.getText().equals(text2.getText()) && text2.getText().equals(text3.getText()))
+                        stopBtn.setText("Вы выиграли. Начать заново");
+                    else
+                        stopBtn.setText("Вы проиграли. Начать заново");
+                    stopBtn.setOnMouseClicked(e4 -> newGame());
+                });
+            });
+        });
+
+        Random random = new Random();
+        model.setInt1(random.nextInt(9) + 1);
+        model.setInt2(random.nextInt(9) + 1);
+        model.setInt3(random.nextInt(9) + 1);
+
+        restartTimers();
+    }
+
+    private void restartTimers(){
+        TimerTask timerTask1 = new TimerTask() {
+            @Override
+            public void run() {
+                if (model.getInt1() < 9)
+                    model.setInt1(model.getInt1() + 1);
+                else
+                    model.setInt1(0);
+                Platform.runLater(() -> text1.setText(model.getInt1() + ""));
+            }
+        };
+        TimerTask timerTask2 = new TimerTask() {
+            @Override
+            public void run() {
+                if (model.getInt2() < 9)
+                    model.setInt2(model.getInt2() + 1);
+                else
+                    model.setInt2(0);
+                Platform.runLater(()->text2.setText(model.getInt2() + ""));
+            }
+        };
+        TimerTask timerTask3 = new TimerTask() {
+            @Override
+            public void run() {
+                if (model.getInt3() < 9)
+                    model.setInt3(model.getInt3() + 1);
+                else
+                    model.setInt3(0);
+                Platform.runLater(()->text3.setText(model.getInt3() + ""));
+            }
+        };
+
+        model.setTimer1(new Timer("timer1"));
+        model.getTimer1().schedule(timerTask1, 0, 150);
+
+        model.setTimer2(new Timer("timer2"));
+        model.getTimer2().schedule(timerTask2, 0, 150);
+
+        model.setTimer3(new Timer("timer3"));
+        model.getTimer3().schedule(timerTask3, 0, 150);
+    }
+}
---
+++ src/sample/models/Model.java
@@ -0,0 +1,60 @@
+package sample.models;
+
+import java.util.Timer;
+
+public class Model {
+    private Timer timer1;
+    private Timer timer2;
+    private Timer timer3;
+    private int int1;
+    private int int2;
+    private int int3;
+
+    public Timer getTimer1() {
+        return timer1;
+    }
+
+    public void setTimer1(Timer timer1) {
+        this.timer1 = timer1;
+    }
+
+    public Timer getTimer2() {
+        return timer2;
+    }
+
+    public void setTimer2(Timer timer2) {
+        this.timer2 = timer2;
+    }
+
+    public Timer getTimer3() {
+        return timer3;
+    }
+
+    public void setTimer3(Timer timer3) {
+        this.timer3 = timer3;
+    }
+
+    public int getInt1() {
+        return int1;
+    }
+
+    public void setInt1(int int1) {
+        this.int1 = int1;
+    }
+
+    public int getInt2() {
+        return int2;
+    }
+
+    public void setInt2(int int2) {
+        this.int2 = int2;
+    }
+
+    public int getInt3() {
+        return int3;
+    }
+
+    public void setInt3(int int3) {
+        this.int3 = int3;
+    }
+}
